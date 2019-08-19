e9a993d0fd6c3835b8358816e44ffab35444490d
Owestgate <54096640+Owestgate@users.noreply.github.com>
2019-08-19 16:29:39 +1000

    Messing with an obstacle
    Tipping floor obstacle

---
+++ Testing first personn/Assets/Scripts/TippingFloor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TippingFloor : MonoBehaviour
+{
+
+    public Animator floorAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //floorAnim.Play("TippingPlatform", 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision playerCol){
+        if (playerCol.gameObject.tag == "Character"){
+        floorAnim.Play("TippingPlatform", 0, 0);
+        Debug.Log("FF");
+        }
+    }
+}

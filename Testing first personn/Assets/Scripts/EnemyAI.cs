d2cbfb85c482eab5d2bbdeef6a5bbc6331a1a307
Owestgate <54096640+Owestgate@users.noreply.github.com>
2019-08-30 23:20:02 +1000

    Tested navmesh, fog

---
+++ Testing first personn/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public GameObject playerCharacter;
+    public NavMeshAgent enemy;
+
+    void Start()
+    {
+        playerCharacter = GameObject.FindGameObjectWithTag("Character");
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.destination = playerCharacter.transform.position;
+    }
+}

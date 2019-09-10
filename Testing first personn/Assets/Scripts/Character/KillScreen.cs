b7e3fc24c377aff6645df59d1ffec0e5ccf6e837
mitchellwright <acmetonto@hotmail.com>
2019-09-10 20:02:10 +1000

    10/09 20:00 - MW Changes: - Added option to disable crouch sprinting. (Forgot to upload earlier)

---
+++ Testing first personn/Assets/Scripts/Character/KillScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillScreen : MonoBehaviour
+{
+    public GameObject enemyObj;
+    public GameObject playerObj;
+    //Name of the screen to transition to
+    public string killScreen;
+    //If currentDist is less than this, you die.
+    public float killDist;
+    //Distance between player and chaser.
+    private float currentDist;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets the current distance, compares, then kills
+        currentDist = Vector3.Distance(playerObj.transform.position, enemyObj.transform.position);
+        if (currentDist < killDist)
+        {
+            //Jump scare animation goes here.
+            
+            SceneManager.LoadScene(killScreen);
+        }
+    }
+}

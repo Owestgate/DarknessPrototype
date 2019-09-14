2164301a29c5c26990e05de5a08b92481a154e36
Lewis <bluestriker26@hotmail.com>
2019-09-15 00:32:33 +1000

    DID STUFF

--- Testing first personn/Assets/Scripts/LightRoomTest.cs
+++ Testing first personn/Assets/Scripts/LightRoomTest.cs
@@ -1,80 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityStandardAssets.Characters.FirstPerson;
-using UnityEngine.AI;
+﻿using UnityEngine;
 
 public class LightRoomTest : MonoBehaviour
 {
-    public GameObject subLights;
-    public bool switchingOn;
-    public float lightTimeOn;
-    public float lightTimeOff;
-    public GameObject player;
-    public GameObject chaser;
+    public bool shouldBypass;
 
-    private float walkSpeedOnStart;
-    private float runSpeedOnStart;
+    public Collider playerCol;
 
-    public float walkSpeedInDarkness;
-    public float runSpeedInDarkness;
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        StartCoroutine(LightsOff());
-        switchingOn = true;
-        runSpeedOnStart = player.GetComponent<FirstPersonController>().m_RunSpeed;
-        walkSpeedOnStart = player.GetComponent<FirstPersonController>().m_WalkSpeed;
+        if (other != playerCol) return;
+        RoomLights.Instance.bypass = shouldBypass;
     }
 
-    IEnumerator LightsOn(){
-        while (true){
-        
-        yield return new WaitForSeconds(lightTimeOff);
-        switchingOn = ! switchingOn;    // bool trigger
-        yield return StartCoroutine(LightsOff());
-        
-        }
-           
-    }
-    IEnumerator LightsOff()
+    private void OnTriggerExit(Collider other)
     {
-        while (true)
-        {
-
-            yield return new WaitForSeconds(lightTimeOn);
-            switchingOn = !switchingOn;    // bool trigger\
-            yield return StartCoroutine(LightsOn());
-
-        }
-
-    }
-
-    void Update()
-    {
-        //Turning lights on/ off through game object active status
-        if (switchingOn == true){
-            subLights.SetActive(true);
-        }
-        if (switchingOn == false){
-            subLights.SetActive(false);
-        }
-    }
-    
-    void OnTriggerStay (Collider other){
-        if (other.gameObject.tag == "Character" && switchingOn == false){
-            //other.gameObject.GetComponent<FirstPersonController>().enabled = false;
-            player.GetComponent<FirstPersonController>().m_RunSpeed = runSpeedInDarkness;
-            player.GetComponent<FirstPersonController>().m_WalkSpeed = walkSpeedInDarkness;
-            player.GetComponent<FirstPersonController>().m_JumpAllowed = false;
-            chaser.GetComponent<EnemyAI>().lightsOn = false; //Lights now simply inform the chaser that the lights are off. Chaser does the work itself now.
-        } 
-        if(other.gameObject.tag == "Character" && switchingOn == true) {
-            player.GetComponent<FirstPersonController>().m_RunSpeed = runSpeedOnStart;
-            player.GetComponent<FirstPersonController>().m_WalkSpeed = walkSpeedOnStart;
-            player.GetComponent<FirstPersonController>().m_JumpAllowed = true;
-            chaser.GetComponent<EnemyAI>().lightsOn = true;
-        }
+        if (other != playerCol) return;
+        RoomLights.Instance.bypass = shouldBypass;
     }
 }

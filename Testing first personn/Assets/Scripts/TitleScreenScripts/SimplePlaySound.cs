29a374bdd6e6b4abf1ba631ad18f10d79027c344
Lewis <bluestriker26@hotmail.com>
2019-09-18 23:21:51 +1000

    Basic Menu animation still abit buggy

---
+++ Testing first personn/Assets/Scripts/TitleScreenScripts/SimplePlaySound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SimplePlaySound : MonoBehaviour
+{
+    public AudioSource HoverSource;
+    public AudioSource PressedSource;
+    public void PlayHoverSound()
+    {
+        HoverSource.Play();
+    }
+    public void PlayPressedSound()
+    {
+         PressedSource.Play();
+    }
+}

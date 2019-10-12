dd3b8ea6d897f574030b64c7917ccc245ca7f620
Owestgate <54096640+Owestgate@users.noreply.github.com>
2019-10-12 12:39:57 +1100

    Light room buttons

--- Testing first personn/Assets/Scripts/LightButton.cs
+++ Testing first personn/Assets/Scripts/LightButton.cs
@@ -8,6 +8,8 @@ public class LightButton : MonoBehaviour
     public GameObject ButtonController;
     public string color;
 
+    public GameObject light;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +25,12 @@ public class LightButton : MonoBehaviour
     public void Press()
     {
         ButtonController.GetComponent<ButtonController>().ButtonPressed(color);
+        StartCoroutine(FlashLight());
+    }
+
+    IEnumerator FlashLight(){
+        light.SetActive (true);
+        yield return new WaitForSeconds(0.3f);
+        light.SetActive (false);
     }
 }

57beba822288dff8d38e6b728bd71fd14ce2a893
Owestgate <54096640+Owestgate@users.noreply.github.com>
2019-10-13 11:15:52 +1100

    Ui additions

---
+++ Testing first personn/Assets/Scripts/Menu/PaintingUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaintingUI : MonoBehaviour
+{
+    public GameObject paintingUI;
+    bool onlyShowOnce = true;
+
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "Character" && onlyShowOnce == true){
+            paintingUI.SetActive(true);
+            StartCoroutine(UIOnScreen());
+            onlyShowOnce = false;
+
+        }
+    }
+
+    IEnumerator UIOnScreen(){
+        yield return new WaitForSeconds(5);
+        paintingUI.SetActive(false);
+        
+    }
+}

7cfa876dfed39faa960b952ebe6149fbb40a8c59
Owestgate <54096640+Owestgate@users.noreply.github.com>
2019-10-29 11:46:22 +1100

    Flare + Mirror off at start intro

---
+++ Testing first personn/Assets/Scripts/Menu/MirrorFlareUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MirrorFlareUI : MonoBehaviour
+{
+    public GameObject canvasUI;
+    public GameObject player;
+
+    public GameObject screenCentre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasUI.SetActive(false);
+        screenCentre.SetActive(false);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.GetComponent<FlareManager3>().canUse == true){
+            canvasUI.SetActive(true);
+            screenCentre.SetActive(true);
+        }
+    }
+}

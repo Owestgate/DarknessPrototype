2ad488db36e1b8fbbce4b5bf69ae745442941cfc
Ewen <102104276@student.swin.edu.au>
2019-10-30 12:01:09 +1100

    deathsound plays through load screen

---
+++ Testing first personn/Assets/preserveNoise.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class preserveNoise : MonoBehaviour
+{
+    private static preserveNoise instance = null;
+
+    public static preserveNoise Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null & instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

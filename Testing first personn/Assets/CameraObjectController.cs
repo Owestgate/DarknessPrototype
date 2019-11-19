46b3350184a52856385d1ead14b6b26ba5e9a1c5
Lewis <bluestriker26@hotmail.com>
2019-11-19 22:45:22 +1100

    New scripts with camera delay

--- Testing first personn/Assets/CameraObjectController.cs
+++ Testing first personn/Assets/CameraObjectController.cs
@@ -1,23 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraObjectController : MonoBehaviour
 {
+    public AudioSource ReadySound;
     public AudioSource FlashSound;
+    public AudioSource BeepSound;
     public Animator Anim;
     public float CameraFlashRate = 0.1f;
     private float nextFlashTime;
+    public Renderer orangeLight;
+
+    public float holdDownTime = 1;
+    private float currentHoldDownTime;
+
+    private void Start()
+    {
+        orangeLight.enabled = false;
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
             if (Time.time > nextFlashTime)
             {
-                nextFlashTime = Time.time + CameraFlashRate;
-                Anim.Play("CameraFlash", 0, 0);
-                FlashSound.Play();
+                orangeLight.enabled = true;
+                ReadySound.Play();
             }
         }
+
+        if (Input.GetMouseButton(1))
+        {
+            if (orangeLight.enabled)
+            {
+                currentHoldDownTime += Time.deltaTime;
+
+                if (currentHoldDownTime >= holdDownTime)
+                {
+                    nextFlashTime = Time.time + CameraFlashRate;
+                    Anim.Play("CameraFlash", 0, 0);
+                    FlashSound.Play();
+                    orangeLight.enabled = false;
+                }
+            }      
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            currentHoldDownTime = 0;
+            orangeLight.enabled = false;
+        }
+    }
+
+    public void PlayBeepSound()
+    {
+        BeepSound.Play();
     }
 }

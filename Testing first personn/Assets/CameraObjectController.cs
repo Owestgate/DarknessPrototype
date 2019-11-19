221d80d44135286522b2ee8a12528955ad96fb67
Lewis <bluestriker26@hotmail.com>
2019-11-19 20:45:58 +1100

    New scripts

---
+++ Testing first personn/Assets/CameraObjectController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraObjectController : MonoBehaviour
+{
+    public AudioSource FlashSound;
+    public Animator Anim;
+    public float CameraFlashRate = 0.1f;
+    private float nextFlashTime;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (Time.time > nextFlashTime)
+            {
+                nextFlashTime = Time.time + CameraFlashRate;
+                Anim.Play("CameraFlash", 0, 0);
+                FlashSound.Play();
+            }
+        }
+    }
+}

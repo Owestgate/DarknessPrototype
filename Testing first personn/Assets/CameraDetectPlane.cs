f92138d0906cd2f2bd5b6b426b1fb6b4a93b1af1
102104276 <102104276@student.swin.edu.au>
2019-11-24 22:07:04 +1100

    detecting camera hit v1 added a test light above the new area so we can see whats going on its at the top of the hierarchy

---
+++ Testing first personn/Assets/CameraDetectPlane.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDetectPlane : MonoBehaviour
+{
+    private Camera target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var n = target.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(n) * Quaternion.Euler(90, 0, 0);
+        transform.localScale = new Vector3(n.magnitude/40, n.magnitude/40, n.magnitude/40);
+    }
+}

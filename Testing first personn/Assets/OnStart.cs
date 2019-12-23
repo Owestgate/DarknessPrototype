f69452feff471bfb1b1b093a61b4e28ead9ae56d
Lewis <bluestriker26@hotmail.com>
2019-12-23 21:05:51 +1100

    Animations for objective text

---
+++ Testing first personn/Assets/OnStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class OnStart : MonoBehaviour
+{
+    public UnityEvent OnStarted;
+    public UnityEvent OnStartedTwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("OnStartDelay", 0.1f);
+    }
+    void OnStartDelay()
+    {
+        OnStarted.Invoke();
+    }
+}
---
+++ Testing first personn/Assets/OnStartTwo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class OnStartTwo : MonoBehaviour
+{
+    public UnityEvent OnStartedTwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("OnStartDelay", 3f);
+    }
+    void OnStartDelay()
+    {
+        OnStartedTwo.Invoke();
+    }
+}
